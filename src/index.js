36a46781559ed7782f8998b4eaa2075d875bd41a
arpit mishra <arpit.mishra.out@gmail.com>
2024-06-10 17:55:43 +0530

    Animation

--- src/index.js
+++ src/index.js
@@ -42,9 +42,10 @@ dropdownMobile.forEach((menu,i)=>{
 menuBtn.addEventListener(`click`,function(e){
     menuBtn.classList.add(`hidden`);
     sidebar.classList.remove(`hidden`);
-})
-
-closeBtn.addEventListener(`click`,function(e){
-    menuBtn.classList.remove(`hidden`);
-    sidebar.classList.add(`hidden`);
+    
+    })
+    
+    closeBtn.addEventListener(`click`,function(e){
+        menuBtn.classList.remove(`hidden`);
+        sidebar.classList.add(`hidden`); 
 })
